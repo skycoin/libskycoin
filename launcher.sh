8668b6679204bb1d68bc18201610223bc08b621a
Mauricio López-Quintana Conesa <dev0005@simelo.tech>
2018-04-08 13:01:11 -0400

    [docker] [skycoin] refs #1184 Changes launcher name, it's only used in docker.

--- launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-COMMAND="skycoin --data-dir $DATA_DIR --wallet-dir $WALLET_DIR $@"
-VOLUME_UID=`stat -c %U $DATA_DIR`
-if [[ $VOLUME_UID = "UNKNOWN" ]] ; then
-    adduser -D -u $VOLUME_UID skycoin
-    su skycoin $COMMAND
-else
-    su $USERNAME $COMMAND
-fi
