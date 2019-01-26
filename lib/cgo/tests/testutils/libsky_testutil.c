6e571516762b5be229ff6901c062dff64dc18128
Maykel Arias Torres <dev0006@simelo.tech>
2019-01-26 00:42:13 -0500

    refs #2 Added function in skytest by copyGoSlice_toGoSlice

--- lib/cgo/tests/testutils/libsky_testutil.c
+++ lib/cgo/tests/testutils/libsky_testutil.c
@@ -277,6 +277,18 @@ int copySlice(GoSlice_* pdest, GoSlice_* psource, int elem_size){
   return SKY_OK;
 }
 
+int copyGoSlice_toGoSlice(GoSlice* pdest, GoSlice_* psource, int elem_size){
+  pdest->len = psource->len;
+  pdest->cap = psource->len;
+  int size = pdest->len * elem_size;
+  pdest->data = malloc(size);
+  if( pdest->data == NULL )
+    return SKY_ERROR;
+  registerMemCleanup( pdest->data );
+  memcpy(pdest->data, psource->data, size );
+  return SKY_OK;
+}
+
 int cutSlice(GoSlice_* slice, int start, int end, int elem_size, GoSlice_* result){
   int size = end - start;
   if( size <= 0)
