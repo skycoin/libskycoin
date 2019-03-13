4c16efd34c99a56525945b4dc9bce8505df79002
Maykel Arias Torres <dev0006@simelo.tech>
2019-03-13 01:18:47 -0400

    [test-libc] refs #102 Repair error in cipher.bitcoin

--- lib/cgo/tests/test_main.c
+++ lib/cgo/tests/test_main.c
@@ -7,7 +7,7 @@ int main(void)
     int number_failed_fork = 0;
     SRunner *sr = srunner_create(cipher_address());
     SRunner *sr_fork = srunner_create(coin_transaction_fork());
-    // srunner_add_suite(sr, cipher_bitcoin());
+    srunner_add_suite(sr, cipher_bitcoin()); //ok
     // srunner_add_suite(sr, cipher_testsuite());
     srunner_add_suite(sr, cipher_crypto()); //ok
     // srunner_add_suite(sr, cipher_encrypt_scrypt_chacha20poly1305());
