cad81f1aa0d4bfc442e82a324b50ea60d007972c
Maykel Arias Torres <dev0006@simelo.tech>
2019-04-10 09:27:44 -0400

    [test-libc]  refs #2 Disable coin.transactins test in MacOS

--- lib/cgo/tests/check_coin.transactions.c
+++ lib/cgo/tests/check_coin.transactions.c
@@ -1112,7 +1112,7 @@ Suite *coin_transaction(void) {
 
   tc = tcase_create("coin.transaction");
   tcase_add_checked_fixture(tc, setup, teardown);
-  // tcase_add_test(tc, TestTransactionVerify); //ok
+  tcase_add_test(tc, TestTransactionVerify); //ok
   tcase_add_test(tc, TestTransactionPushOutput); //ok
   tcase_add_test(tc, TestTransactionHash); //ok
   tcase_add_test(tc, TestTransactionUpdateHeader); //ok
