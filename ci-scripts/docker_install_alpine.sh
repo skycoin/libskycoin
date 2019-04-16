687a509b6bff7b2216bfd4f90b8c71353f0fe109
Maykel Arias Torres <dev0006@simelo.tech>
2019-04-16 15:34:13 -0400

    [docker-sh] refs #11 Update script alpine

--- ci-scripts/docker_install_alpine.sh
+++ ci-scripts/docker_install_alpine.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apk update
-apk add --upgrade fts-dev fts build-base gcc git curl wget check-dev alpine-sdk make g++ zlib-dev
+apk add --upgrade fts-dev build-base gcc git curl wget check-dev alpine-sdk make g++ zlib-dev
